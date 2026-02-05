2f90943fa0438228119d864d5b4e7c0f1385e0aa
andres-maldonado <155512647+andres-maldonado@users.noreply.github.com>
2026-02-05 16:57:57 -0500

    Lever

--- Assets/Scripts/Work/Minigames/Lever/Lever.cs
+++ Assets/Scripts/Work/Minigames/Lever/Lever.cs
@@ -6,8 +6,13 @@ using UnityEngine.UIElements;
 public class Lever : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] Transform pivotPoint;
+<<<<<<< Updated upstream
     [SerializeField] float moveSpeed;
     [SerializeField] float leverMin, leverMax;
+=======
+    [SerializeField] float moveSpeed, angleMin, angleMax;
+
+>>>>>>> Stashed changes
     [SerializeField] InputActionAsset input;
     private InputActionMap map;
     private InputAction look;
@@ -26,11 +31,19 @@ public class Lever : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPoi
     // Update is called once per frame
     void FixedUpdate()
     {
+<<<<<<< Updated upstream
         DragLever();
+=======
+        if (isDragging && !isHovering)
+        {
+            DragLever();
+        }
+>>>>>>> Stashed changes
     }
     public void OnPointerDown(PointerEventData eventData)
     {
         isDragging = true;
+        //Debug.Log("Dragging: "+isDragging+", Hovering: "+isHovering);
     }
     public void OnPointerUp(PointerEventData eventData)
     {
@@ -43,6 +56,7 @@ public class Lever : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPoi
     public void OnPointerExit(PointerEventData eventData)
     {
         isHovering = false;
+<<<<<<< Updated upstream
     }
     public void DragLever()
     {
@@ -51,12 +65,40 @@ public class Lever : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPoi
             moveDir = moveSpeed;
         }
         else if(look.ReadValue<Vector2>().y < 0)
+=======
+        if (look.ReadValue<Vector2>().y > 0)
+>>>>>>> Stashed changes
         {
             moveDir = -moveSpeed;
         }
+        else if (look.ReadValue<Vector2>().y < 0)
+        {
+            moveDir = moveSpeed;
+        }
+    }
+    public void DragLever()
+    {
         if (isDragging && !isHovering)
         {
+<<<<<<< Updated upstream
             pivotPoint.localEulerAngles += new Vector3(moveDir, 0, 0);
+=======
+            if ((pivotPoint.localEulerAngles.x <= angleMax && moveDir < 0) || (pivotPoint.localEulerAngles.x >= angleMin && moveDir > 0))
+            {
+                pivotPoint.eulerAngles += new Vector3(moveDir, 0, 0);
+                Debug.Log(pivotPoint.localRotation);
+            }
+        }
+        if(pivotPoint.localEulerAngles.x <= angleMin)
+        {
+            pivotPoint.localEulerAngles = new Vector3(angleMin, 0, 0);
+            isDragging = false;
+        }
+        if (pivotPoint.localEulerAngles.x >= angleMax)
+        {
+            pivotPoint.localEulerAngles = new Vector3(angleMax, 0, 0);
+            isDragging = false;
+>>>>>>> Stashed changes
         }
     }
 }

014382500748203c3a602e95bec85835e967e02d
andres-maldonado <155512647+andres-maldonado@users.noreply.github.com>
2026-02-09 17:25:00 -0500

    Final Button Animated

--- Assets/Scripts/Work/General/FinalButton.cs
+++ Assets/Scripts/Work/General/FinalButton.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class FinalButton : WorkButton
 {
     [SerializeField] MinigameManager manager;
+    [SerializeField] Animator zoomAnim;
     public bool ready;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
@@ -11,6 +12,7 @@ public class FinalButton : WorkButton
     }
     public override void OnClick()
     {
+        zoomAnim.Play("FinalButtonHit");
         Debug.Log("Ready: " + ready);
         if (ready)
         {

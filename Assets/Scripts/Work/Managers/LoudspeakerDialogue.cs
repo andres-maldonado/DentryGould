3e6dd45883860c405d08987be3b883b75a64bd35
andres-maldonado <155512647+andres-maldonado@users.noreply.github.com>
2026-03-02 00:24:31 -0500

    Work Text

--- Assets/Scripts/Work/Managers/LoudspeakerDialogue.cs
+++ Assets/Scripts/Work/Managers/LoudspeakerDialogue.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
 using System.Collections;
+using FMODUnity;
 
 public class LoudspeakerDialogue : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class LoudspeakerDialogue : MonoBehaviour
     [SerializeField] float writeSpeed, pauseTime, blankTime;
     [SerializeField] TextMeshPro bottomText;
     [SerializeField] DoorButton door;
+    [SerializeField] EventReference dialogueSound;
 
     private string[] dialogueByLine;
     private Queue<string> dialogueLines = new Queue<string>();

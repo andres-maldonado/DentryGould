81677c7d21e4e8b47965c14d18944403705268da
andres-maldonado <155512647+andres-maldonado@users.noreply.github.com>
2026-04-06 23:35:14 -0400

    MainMenu Jitter

--- Assets/Scripts/General/MainMenuScreenEffect.cs
+++ Assets/Scripts/General/MainMenuScreenEffect.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class MainMenuScreenEffect : MonoBehaviour
 {
+    [SerializeField] float vRange, hRange, fadeMin, fadeMax, changeRate;
+    [SerializeField] SpriteRenderer glowControl;
+
+    private float counter;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,15 @@ public class MainMenuScreenEffect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (counter < 0)
+        {
+            gameObject.transform.localPosition = new Vector3(Random.Range(-hRange, hRange), Random.Range(-vRange, vRange));
+            glowControl.color = new Color32(0, 0, 0, (byte)Random.Range(fadeMin, fadeMax));
+            counter = changeRate;
+        }
+        else
+        {
+            counter -= Time.deltaTime;
+        }
     }
 }
